fb334ffb1cbefbb4fd95175f911a002757c5657b
AJGeo <aj_bresler@yahoo.co.uk>
2021-01-13 00:07:00 +0200

    Update the FilterModel

--- src/main/java/org/aj/lists/filter/model/FilterModel.java
+++ src/main/java/org/aj/lists/filter/model/FilterModel.java
@@ -1,5 +1,6 @@
 package org.aj.lists.filter.model;
 
+import org.aj.database.common.IDataTable;
 import org.aj.lists.api.FilterColumnsEnum;
 import org.aj.lists.api.IFilterModel;
 
@@ -8,6 +9,12 @@ import java.util.Map;
 import java.util.Optional;
 
 public class FilterModel implements IFilterModel {
+    private final IDataTable dataTable;
+
+    public FilterModel(IDataTable dataTable) {
+        this.dataTable = dataTable;
+    }
+
     @Override
     public final Optional<Map<FilterColumnsEnum, Optional<List<String>>>> Filter(String groupFilterValue,
                                                                                  String typeFilterValue,
