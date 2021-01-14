a76aa4a94882ad6b38d26f2f5651f6dddcd66d86
AJGeo <aj_bresler@yahoo.co.uk>
2021-01-14 22:24:17 +0200

    Use data source for testing class

--- src/test/java/org/aj/lists/filter/model/FilterModelTest.java
+++ src/test/java/org/aj/lists/filter/model/FilterModelTest.java
@@ -13,60 +13,23 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilterModelTest {
+    private IDataTable dataTable;
+    private DataSourceForTest dataSourceForTest;
+
+    public FilterModelTest() {
+        dataSourceForTest = new DataSourceForTest();
+        dataTable = Mockito.mock(IDataTable.class);
+    }
 
     @Test
     public void createModel() {
         System.out.println("Create FilterModel");
 
-        IDataTable dataTable = Mockito.mock(IDataTable.class);
-        setupDataSource(dataTable);
+        dataTable = Mockito.mock(IDataTable.class);
+        dataSourceForTest.setupDataSource(dataTable);
 
         IFilterModel filterModel = new FilterModel(dataTable);
         assertNotNull(filterModel);
     }
 
-    private void setupDataSource(IDataTable dataTable) {
-        Mockito.when(dataTable.getColumnIndex("group")).thenReturn(0);
-        Mockito.when(dataTable.getColumnIndex("type")).thenReturn(1);
-        Mockito.when(dataTable.getColumnIndex("subType")).thenReturn(2);
-
-        List<IDataRow> dataRows = Arrays.asList(
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"),
-                createDataRow("Aircraft", "Fighter", "F16"));
-
-        Mockito.when(dataTable.getRows()).thenReturn(java.util.Optional.of(dataRows));
-    }
-
-    private IDataRow createDataRow(String... strings) {
-        IDataRow dataRow = Mockito.mock(IDataRow.class);
-        Mockito.when(dataRow.getString(0)).thenReturn(java.util.Optional.ofNullable(strings[0]));
-        Mockito.when(dataRow.getString(1)).thenReturn(java.util.Optional.ofNullable(strings[1]));
-        Mockito.when(dataRow.getString(2)).thenReturn(java.util.Optional.ofNullable(strings[2]));
-
-        return dataRow;
-    }
 }
