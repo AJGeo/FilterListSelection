05828ce9f75e55dc98c5532e3f09fde13c19fc78
AJGeo <aj_bresler@yahoo.co.uk>
2021-01-16 04:34:04 +0200

    Expand Model workings

--- src/main/java/org/aj/lists/filter/model/FilterModel.java
+++ src/main/java/org/aj/lists/filter/model/FilterModel.java
@@ -1,15 +1,18 @@
 package org.aj.lists.filter.model;
 
+import org.aj.database.common.IDataRow;
 import org.aj.database.common.IDataTable;
 import org.aj.lists.api.FilterColumnsEnum;
 import org.aj.lists.api.IFilterDataColumnNames;
 import org.aj.lists.api.IFilterModel;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class FilterModel implements IFilterModel {
     private final IDataTable dataTable;
     private final IFilterDataColumnNames filterDataColumnNames;
+    private final List<List<String>> filteredList;
     private int familyColumnIndex;
     private int groupColumnIndex;
     private int typeColumnIndex;
@@ -20,27 +23,88 @@ public class FilterModel implements IFilterModel {
 
         this.dataTable = dataTable;
         this.filterDataColumnNames = filterDataColumnNames;
+        filteredList = new ArrayList<>();
+
+        retrieveColumnIndexesFromDataTable();
 
         filter(null, null, null);
     }
 
-    private final void retriveColumnIndexsFromDataTable() {
-//         familyColumnIndex = dataTable.getColumnIndex(FAMILY_DB_COLUMN);
-//         groupColumnIndex = dataTable.getColumnIndex(GROUP_DB_COLUMN);
-//         typeColumnIndex = dataTable.getColumnIndex(TYPE_DB_COLUMN);
-
+    private final void retrieveColumnIndexesFromDataTable() {
+        familyColumnIndex = dataTable.getColumnIndex(filterDataColumnNames.getColumnName(FilterColumnsEnum.Family));
+        groupColumnIndex = dataTable.getColumnIndex(filterDataColumnNames.getColumnName(FilterColumnsEnum.Group));
+        typeColumnIndex = dataTable.getColumnIndex(filterDataColumnNames.getColumnName(FilterColumnsEnum.Type));
     }
 
     @Override
     public final Optional<Map<FilterColumnsEnum, Optional<List<String>>>> filter(String familyFilterValue,
                                                                                  String groupFilterValue,
                                                                                  String typeFilterValue) {
-        Set<String> familySet = new HashSet();
-        Set<String> groupSet = new HashSet<>();
-        Set<String> typeSet = new HashSet<>();
+        if (!dataTable.getRows().isPresent())
+            return Optional.empty();
 
+        Set<String> familySet = new HashSet<>();
+        familySet.add("");
+        Set<String> groupSet = new HashSet<>();
+        groupSet.add("");
+        Set<String> typeSet = new HashSet<>();
+        typeSet.add("");
+
+        filteredList.clear();
+        for (IDataRow row : dataTable.getRows().get()) {
+            String familyDataValue = row.getString(familyColumnIndex).isPresent() ?
+                    row.getString(familyColumnIndex).get() : null;
+            String groupDataValue = row.getString(groupColumnIndex).isPresent() ?
+                    row.getString(groupColumnIndex).get() : null;
+            String typeDataValue = row.getString(typeColumnIndex).isPresent() ?
+                    row.getString(typeColumnIndex).get() : null;
+
+            if (isFamilyFilterPass(familyDataValue, familyFilterValue) &&
+                    isGroupFilterPass(groupDataValue, groupFilterValue) &&
+                    isTypeFilterPass(typeDataValue, typeFilterValue)) {
+                Optional.ofNullable(familyDataValue).ifPresent(familySet::add);
+                Optional.ofNullable(groupDataValue).ifPresent(groupSet::add);
+                Optional.ofNullable(typeDataValue).ifPresent(typeSet::add);
+
+                addFilteredListValues(familyDataValue, groupDataValue, typeDataValue);
+            }
+        }
+        sortFilteredListValues();
+
+        List<String> familyList = familySet.stream().sorted().collect(Collectors.toList());
+        List<String> groupList = groupSet.stream().sorted().collect(Collectors.toList());
+        List<String> typeList = typeSet.stream().sorted().collect(Collectors.toList());
 
         return null;
+    }
+
+    private boolean isFamilyFilterPass(String dataValue, String familyFilterValue) {
+        return familyFilterValue == null || dataValue.equals(familyFilterValue);
+    }
+
+    private boolean isGroupFilterPass(String dataValue, String groupFilterValue) {
+        return groupFilterValue == null || dataValue.equals(groupFilterValue);
+    }
+
+    private boolean isTypeFilterPass(String dataValue, String typeFilterValue) {
+        return typeFilterValue == null || dataValue.equals(typeFilterValue);
+    }
+
+    private void addFilteredListValues(String... dataValues) {
+        filteredList.add(new ArrayList<>(
+                Arrays.asList(dataValues)));
+    }
+
+    private void sortFilteredListValues() {
+        if (filteredList.isEmpty())
+            return;
+
+        Comparator<List<String>> compareFirstToLastListValue = Comparator.comparing(familyValue -> familyValue.get(0));
+        for (int count = 1; count < filteredList.get(0).size(); ++count) {
+            final int countFinal = count;
+            compareFirstToLastListValue = compareFirstToLastListValue.thenComparing(groupValue -> groupValue.get(countFinal));
+        }
+        filteredList.sort(compareFirstToLastListValue);
     }
 
     @Override
