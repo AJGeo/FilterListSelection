380a769122c342e37f951ec40f9744ee0568bdcd
AJGeo <aj_bresler@yahoo.co.uk>
2021-01-20 01:33:58 +0200

    Add getFilterColunmData function

--- src/test/java/org/aj/lists/filter/model/FilterModelUtil.java
+++ src/test/java/org/aj/lists/filter/model/FilterModelUtil.java
@@ -1,2 +1,19 @@
-package org.aj.lists.filter.model;public class FilterModelUtil {
+package org.aj.lists.filter.model;
+
+import org.aj.lists.api.FilterColumnsEnum;
+import org.aj.lists.api.IFilterEquipmentDataModel;
+
+import java.util.List;
+import java.util.Optional;
+
+class FilterModelUtil {
+    static String[] getFilterColumnData(IFilterEquipmentDataModel filterEquipmentDataModel,
+                                         FilterColumnsEnum filterColumns) {
+        Optional<List<String>> optional = filterEquipmentDataModel.getFilteredColumnsData().get(filterColumns);
+        if (optional.isPresent()) {
+            List<String> FilteredColumnsData = optional.get();
+            return FilteredColumnsData.toArray(new String[0]);
+        }
+        return null;
+    }
 }
